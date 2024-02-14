38dc0728f0e9f0af0f215593178fd7e7a58a9518
Umar Hoshimov <umarkhashimov05@gmail.com>
2024-02-14 11:00:26 +0500

    get data fix

--- core/handlers.py
+++ core/handlers.py
@@ -6,16 +6,21 @@ from .local_setting import API_HOST
 
 async def getMovie(message: Message):
     id = message.text
-    response = requests.get(f'{API_HOST}/api/getmovie/{id}').json()
-    image = URLInputFile(
-        url=API_HOST + response['poster'],
-        filename='poster.png'
-    )
-    video = URLInputFile(
-        url=API_HOST + response['video'],
-    )
-    print(response['id'])
-    print(API_HOST + response['poster'])
-    await message.answer_photo(photo=image)
-    await message.answer_video(video=video)
-    await message.answer(message.text)
+
+    response = requests.get(f'{API_HOST}/api/getmovie/{id}')
+    if response.status_code == 404:
+
+        response = 'Not found 404'
+        await message.answer(response)
+
+    else:
+        
+        data = response.json()
+        image = URLInputFile(
+            url=API_HOST + data['poster'],
+            filename='poster.png'
+        )
+        
+        caption = f"Title : {data['title']}\nGenres : {data['genres']}\nYear : {data['year']}\n\nDescription :\n{data['description']}"
+        
+        await message.answer_photo(photo=image, caption=caption)
