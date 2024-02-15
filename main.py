00e103139361203981b7028691117c9d101edbea
Umar Hoshimov <umarkhashimov05@gmail.com>
2024-02-15 18:28:17 +0500

    fix

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+from aiogram.filters import CommandStart
 
 import core.local_setting as st
 from core.handlers import getMovie
 from core.callback import getVideo
+
+async def greeting(message: Message, bot: Bot):
+    await message.answer("Welcome")
 
 async def start():
     logging.basicConfig(level=logging.INFO)
     bot = Bot(token=st.TOKEN)
     dp = Dispatcher()
 
+    dp.message.register(greeting, CommandStart())
     dp.message.register(getMovie)
     dp.callback_query.register(getVideo)
 
